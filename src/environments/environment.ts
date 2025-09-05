d3f3a258120c6ec1c5a573db7e5664066635738c
Nabil-FRA <nabil.ait.radi1997@gmail.com>
2025-09-06 00:58:24 +0200

    Initial commit du projet frontend2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.votre-domaine.com/api' // <-- URL de votre future API en ligne
+  apiUrl: 'https://mon-api-stock-2025-6c6acb6eca14.herokuapp.com/' // <-- URL de votre future API en ligne
 };
