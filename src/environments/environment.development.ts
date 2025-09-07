b25643bf93d921c7165b6f2935a046641b1d4828
Nabil-FRA <nabil.ait.radi1997@gmail.com>
2025-09-07 21:14:15 +0200

    Suppression du proxy CORS

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://cors-anywhere.herokuapp.com/https://mon-api-stock-2025-6ca6bce4-14.herokuapp.com/loginCheck'
+  apiUrl: 'https://mon-api-stock-2025-6ca6bce4-14.herokuapp.com/loginCheck'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://cors-anywhere.herokuapp.com/https://mon-api-stock-2025-6ca6bce4-14.herokuapp.com/loginCheck' // <-- URL de votre future API en ligne
+  apiUrl: 'https://mon-api-stock-2025-6ca6bce4-14.herokuapp.com/loginCheck'
 };
