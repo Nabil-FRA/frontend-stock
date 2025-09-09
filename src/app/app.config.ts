ce309e95911dbf427729ec5625da5484c12d527d
Nabil-FRA <nabil.ait.radi1997@gmail.com>
2025-09-09 22:48:02 +0200

    FIX: Add /api prefix to all API service calls9

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,18 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
+// 1. Importer les fonctions nécessaires
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+// 2. Importer votre intercepteur
 import { authInterceptor } from './core/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    // Cette fonction assure que l'intercepteur est appliqué à TOUTES les requêtes HttpClient
-    provideHttpClient(
-      withInterceptors([authInterceptor])
-    )
+
+    // 3. Activer l'intercepteur pour toutes les requêtes HTTP
+    provideHttpClient(withInterceptors([authInterceptor]))
   ]
 };
+
